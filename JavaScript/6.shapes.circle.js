b36b36d0038f71e31589c9f282da47df90585d63
cericube <cericube@naver.com>
2025-12-01 12:59:51 +0900

    반복문까지

---
+++ JavaScript/6.shapes.circle.js
@@ -0,0 +1,10 @@
+// shapes/circle.js
+export class Circle {
+  constructor(radius) {
+    this.radius = radius;
+  }
+
+  area() {
+    return Math.PI * this.radius ** 2;
+  }
+}
---
+++ JavaScript/6.shapes.rectangle.js
@@ -0,0 +1,11 @@
+// shapes/rectangle.js
+export class Rectangle {
+  constructor(width, height) {
+    this.width = width;
+    this.height = height;
+  }
+
+  area() {
+    return this.width * this.height;
+  }
+}
